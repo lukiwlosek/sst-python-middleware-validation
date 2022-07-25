7b8c1a8e78cf5dc5664c951546a920dc65932594
Dualem <pan.lukasz.wlosek@gmail.com>
2022-07-25 21:37:56 +0100

    Switching from powertools validator to openapi validator

--- services/functions/handler.py
+++ services/functions/handler.py
@@ -4,7 +4,6 @@ from aws_lambda_powertools import Logger
 from aws_lambda_powertools.event_handler import APIGatewayRestResolver
 from aws_lambda_powertools.logging import correlation_paths
 from aws_lambda_powertools.utilities.typing import LambdaContext
-from aws_lambda_powertools.utilities.validation import validator, envelopes, validate
 
 import services.functions.schemas as schemas
 
@@ -13,13 +12,33 @@ logger = Logger(service="APP")
 app = APIGatewayRestResolver()
 
 
+from openapi_spec_validator import validate_spec
+from openapi_spec_validator.readers import read_from_filename
+from openapi_schema_validator import validate
+
+spec_dict, spec_url = read_from_filename("./services/assets/api-spec.yaml")
+
+# If no exception is raised by validate_spec(), the spec is valid.
+# validate_spec(spec_dict)
+
+
 @app.post("/test")
 def hello_name():
+
+    print(
+        validate(
+            app.current_event.json_body,
+            spec_dict["paths"]["/test"]["post"]["requestBody"]["content"][
+                "application/json"
+            ]["schema"],
+        )
+    )
     return {"message": f"hello !"}
 
 
 @app.get("/test")
 def hello():
+    # print(validate(app.current_event, spec_dict))
     logger.info(f"Request from GET received")
     return {"message": "hello unknown!"}
 
@@ -30,21 +49,20 @@ def hello():
 def handler(event: dict, context: LambdaContext):
     event["path"] = event["requestContext"]["http"]["path"]
     event["httpMethod"] = event["requestContext"]["http"]["method"]
-
     # validate body schemas
-    validate(
-        event=event,
-        schema=schemas.INPUT["paths"]["/test"]["post"]["requestBody"]["content"][
-            "application/json"
-        ]["schema"],
-        envelope=envelopes.API_GATEWAY_HTTP,
-    )
+    # validate(
+    #     event=event,
+    #     schema=schemas.INPUT["paths"]["/test"]["post"]["requestBody"]["content"][
+    #         "application/json"
+    #     ]["schema"],
+    #     envelope=envelopes.API_GATEWAY_HTTP,
+    # )
 
-    # validate headers
-    validate(
-        event=event,
-        schema=schemas.INPUT["paths"]["/test"]["parameters"],
-        envelope="headers",
-    )
+    # # validate headers
+    # validate(
+    #     event=event,
+    #     schema=schemas.INPUT["paths"]["/test"]["parameters"],
+    #     envelope="headers",
+    # )
 
     return app.resolve(event, context)
